1793f61197417cb73f8443e0433238af01d913c2
Marcelo de Carvalho Lopes <marcelolcarvalho@gmail.com>
2026-09-23 17:24:21 -0300

    Stream sorted

---
+++ src/main/java/com/algaworks/estoque/MainOrdenandoElementos.java
@@ -0,0 +1,22 @@
+package com.algaworks.estoque;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MainOrdenandoElementos {
+
+    public static void main(String[] args) {
+        var cadastroProduto = new CadastroProduto();
+        List<Produto> produtos = cadastroProduto.obterTodos();
+
+        produtos.stream()
+                .filter(Produto::temEstoque)
+                .sorted(Comparator.comparingInt(Produto::getQuantidade))
+                .forEach(produto -> System.out.printf("%s = %d unidades%n",
+                        produto.getNome(), produto.getQuantidade()));
+
+
+
+    }
+}
+
