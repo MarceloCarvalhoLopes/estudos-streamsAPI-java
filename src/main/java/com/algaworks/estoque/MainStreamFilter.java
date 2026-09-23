ca314a2c734c919a48ac14debc3ed16e0dbe2994
Marcelo de Carvalho Lopes <marcelolcarvalho@gmail.com>
2026-09-23 15:23:14 -0300

    Stream filter

---
+++ src/main/java/com/algaworks/estoque/MainStreamFilter.java
@@ -0,0 +1,21 @@
+package com.algaworks.estoque;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class MainStreamFilter {
+
+    public static void main(String[] args) {
+        var cadastroProduto = new CadastroProduto();
+        List<Produto> produtos = cadastroProduto.obterTodos();
+
+        Stream<Produto> stream = produtos.stream();
+        Stream<Produto> streamComEstoque = stream
+                .filter(Produto::temEstoque);
+
+        Stream<Produto> streamComEstoqueInativo = streamComEstoque
+                .filter(Produto::isInativo);
+
+    }
+}
+
